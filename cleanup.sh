0be397458cda4762058f27512c9e66f9c95b1d54
joshr <joshr@joshr.com>
2015-03-08 17:55:52 -0400

    new

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# .cleanup.sh.swp  .gitignore  Makefile.am  aclocal.m4     cleanup.sh*  configure*    src/
+# .git/        LICENSE     README.md    autom4te.cache/  config.log   configure.ac
+
+rm -f aclocal.mv configure config.log
+rm -rf autom4te.cache
+
+
