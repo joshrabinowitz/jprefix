2d542343af5503487e12dd53c81a3a897bf9e1f7
joshr <joshr@joshr.com>
2015-03-08 18:04:19 -0400

    mods

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,14 @@
 # .cleanup.sh.swp  .gitignore  Makefile.am  aclocal.m4     cleanup.sh*  configure*    src/
 # .git/        LICENSE     README.md    autom4te.cache/  config.log   configure.ac
 
-rm -f aclocal.mv configure config.log
+# COPYING@        aclocal.m4      configure.ac
+#INSTALL@        autogen.sh*     depcomp@
+#LICENSE         cleanup.sh*     install-sh@
+#Makefile.am     compile@        missing@
+#README.md       config.status*      src/
+
+rm -f aclocal.m4 configure config.log config.status
 rm -rf autom4te.cache
+rm -f COPYING INSTALL  depcomp install-sh compile missing
 
 
