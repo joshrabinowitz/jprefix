d93d4ea97732e3faacf2f87f202f66f53ec07846
joshr <joshr>
2015-03-09 19:32:42 -0400

    spacing change

--- jprefix.h
+++ jprefix.h
@@ -1,6 +1,6 @@
 #include <string>
 #include <iostream>  /* for std::istream */
-#include <vector>     /* for vector<type> */
+#include <vector>    /* for vector<type> */
 #include "jpopts.h" /* for JPrefixOptoins */
 
 int copy_stream_prefixed (std::istream &in, JPrefixOptions opts);
