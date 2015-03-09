6fcd1c4dc60869c90237ded861e36d443f49f6e8
joshr <joshr>
2015-03-08 23:57:04 -0400

    tighten up regexes. improve test output.

--- test-jprefix.pl
+++ test-jprefix.pl
@@ -7,25 +7,30 @@ print "PASS: test1\n";
 
 # tests to compare output of jprefix with expected output
 my @tests = (
-        # "TEST COMMAND",                     , CMP , "EXPECTED OUTPUT"
+        # "test name",  "TEST COMMAND",                     , CMP , "EXPECTED OUTPUT"
     [ "echo x | ./jprefix --text y",            "eq", "y x" ],
+    [ "echo x | ./jprefix --text=y",            "eq", "y x" ],
     [ "echo x | ./jprefix --text ''",           "eq", "x" ],    # edge case
     [ "echo x | ./jprefix --text ' '",          "eq", "  x" ],  # edge case
-    [ "echo x | ./jprefix --text y --hostname", "=~", 'y \S+ x' ],
-    [ "echo x | ./jprefix --text y --timestamp", "=~", 'y \S+ \S+ x' ],
-    [ "echo x | ./jprefix --text y --utimestamp", "=~", 'y \S+ \S+ x' ],
+    [ "echo x | ./jprefix --text y --hostname", "=~", '^y \S+ x$' ],
+    [ "echo x | ./jprefix --text y --timestamp", "=~", '^y \S+ \S+ x$' ],
+    [ "echo x | ./jprefix --text y --utimestamp", "=~", '^y \S+ \S+ x$' ],
+
+    [ "./jprefix README.md | head -1",            '=~', '^# jprefix$' ],
+    [ "./jprefix --text y README.md | head -1",   '=~', '^y # jprefix$' ],
 );
 
 print "1.." . scalar(@tests) . "\n";    # TAP header
 
 my @results = ();
+my $test_num = 0;
 for my $test (@tests) {
+    $test_num++;
     my ($cmd, $cmp, $expected) = @$test;
-    my $out = `$cmd`;
-    chomp($out);
+    chomp( my $out = `$cmd` );
     my $ok;
     eval "\$ok = \$out $cmp \$expected";
-    printf( "%s: ('$out' $cmp '$expected')\n", $ok ? "OK" : "NOT OK" );
+    printf( "%s: test$test_num ('$out' $cmp '$expected')\n", $ok ? "OK" : "NOT OK" );
     push(@results, $ok);
 }
 my @fails = grep { !$_ } @results;
