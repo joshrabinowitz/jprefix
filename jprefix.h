faa635e6c9a2313cf25c2a236d7617b0c27766ac
joshr <joshr>
2015-03-09 11:04:16 -0400

    code moved to jpdateutils.cc/jpdateutils.h

--- jprefix.h
+++ jprefix.h
@@ -24,12 +24,3 @@ int copy_stream_prefixed (std::istream &in, JPrefixOptions opts);
 const std::string get_usage();
 const std::string myjoin( const std::string joiner, const std::vector<std::string> array );
 const std::string get_hostname();
-const std::string get_date_time();
-const std::string get_date_utime();
-const std::string get_time_elapsed();
-const timeval get_time_elapsed_timeval();
-
-// returns sign, puts result in *result, modified *x and *y
-int timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y); 
-void dump_timeval( const std::string label, const timeval &timeval );
-const std::string get_timeval_as_string (const timeval &t);
