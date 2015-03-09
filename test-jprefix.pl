b1de7b6b76fe0cf8ce997025dfd9bf1c78137350
joshr <joshr>
2015-03-08 21:13:53 -0400

    new

---
+++ test-jprefix.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl -w
+use strict;
+
+# test system, as per https://www.gnu.org/software/automake/manual/html_node/Scripts_002dbased-Testsuites.html#Scripts_002dbased-Testsuites
+#
+print "PASS: test1\n";
+exit(0);
