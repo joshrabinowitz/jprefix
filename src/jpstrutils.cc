f5d8c7a3bdd6af8f2e88141d28ef73ee6a937f9c
joshr <joshr@joshr.com>
2015-03-12 10:30:26 -0400

    reorder code

--- src/jpstrutils.cc
+++ src/jpstrutils.cc
@@ -5,20 +5,20 @@
 const std::string myjoin( const std::string joiner, const std::vector<std::string> array ) 
 {
     std::string str = "";
-    if (0) {
-        // simpler use-int-as-iterator version. 6 short lines
-        for(unsigned int i=0; i<array.size(); i++) {
-            str += array[i];
-            if(i < array.size() - 1) {
-                str += joiner;
-            }
-        }
-    } else {
+    if (1) {
         // STL iterator version. 
         for (std::vector<std::string>::const_iterator it = array.begin(); it != array.end(); it++) {
             str += *it;
             std::vector<std::string>::const_iterator next = it;
             if(++next != array.end()) {
+                str += joiner;
+            }
+        }
+    } else {
+        // simpler use-int-as-iterator version. 6 short lines
+        for(unsigned int i=0; i<array.size(); i++) {
+            str += array[i];
+            if(i < array.size() - 1) {
                 str += joiner;
             }
         }
