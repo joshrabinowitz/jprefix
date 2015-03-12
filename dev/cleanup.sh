839cd316137dc61aae00a4bff0860bf376ac4aa3
joshr <joshr@joshr.com>
2015-03-12 09:59:30 -0400

    improve

--- dev/cleanup.sh
+++ dev/cleanup.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env perl
+use strict;
+use warnings;
 
 # .cleanup.sh.swp  .gitignore  Makefile.am  aclocal.m4     cleanup.sh*  configure*    src/
 # .git/        LICENSE     README.md    autom4te.cache/  config.log   configure.ac
@@ -9,11 +11,20 @@
 #Makefile.am     compile@        missing@
 #README.md       config.status*      src/
 
+if (-f "Makefile") {
+    system( "make clean" );
+}
+for my $d (<DATA>) {
+    chomp($d);
+    system( $d );
+}
+
+__DATA__
 rm -f aclocal.m4 configure config.log config.status
 rm -rf autom4te.cache src/.deps
 rm -f depcomp install-sh compile missing
 rm -f Makefile Makefile.in  src/Makefile src/Makefile.in
-rm -f src/*.o src/.dirstamp
+rm -f src/*.o src/.dirstamp src/jprefix
 rm -f test/test-jprefix.pl.*  test-suite.log  test-driver
 
 
