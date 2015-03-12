c31d7c0f63f97fe6a6d84ceb518febb9b178ea50
joshr <joshr@joshr.com>
2015-03-12 10:07:33 -0400

    use jprefix from '.'  Also test -v option

--- test/test-jprefix.pl
+++ test/test-jprefix.pl
@@ -6,26 +6,28 @@ use strict;
 # tests to compare output of jprefix with expected output
 my @tests = (
        # "TEST COMMAND",                          CMP,     "EXPECTED OUTPUT"
-    [ "echo x | src/jprefix --text y",              "eq",    "y x" ],
-    [ "echo x | src/jprefix --text=y",              "eq",    "y x" ],
-    [ "echo x | src/jprefix --text=y --suffix",     "eq",    "x y" ],
-    [ "echo x | src/jprefix --text ''",             "eq",    "x" ],    # edge case
-    [ "echo x | src/jprefix --text ' '",            "eq",    "  x" ],  # edge case
-    [ "echo x | src/jprefix --text y --hostname",   "=~",    '^y \S+ x$' ],
-    [ "echo x | src/jprefix --text y --timestamp",  "=~",    '^y \S+ \S+ x$' ],
-    [ "echo x | src/jprefix --text y --utimestamp", "=~",    '^y \S+ \S+ x$' ],
+    [ "echo x | ./jprefix --text y",              "eq",    "y x" ],
+    [ "echo x | ./jprefix --text=y",              "eq",    "y x" ],
+    [ "echo x | ./jprefix --text=y --suffix",     "eq",    "x y" ],
+    [ "echo x | ./jprefix --text ''",             "eq",    "x" ],    # edge case
+    [ "echo x | ./jprefix --text ' '",            "eq",    "  x" ],  # edge case
+    [ "echo x | ./jprefix --text y --hostname",   "=~",    '^y \S+ x$' ],
+    [ "echo x | ./jprefix --text y --timestamp",  "=~",    '^y \S+ \S+ x$' ],
+    [ "echo x | ./jprefix --text y --utimestamp", "=~",    '^y \S+ \S+ x$' ],
 
-    [ "echo A | src/jprefix -s -t B -u",            '=~',    '^A B \S+ \S+$' ],
-    [ "echo A | src/jprefix --suffix --text B --utime",'=~', '^A B \S+ \S+$' ],
+    [ "echo A | ./jprefix -s -t B -u",            '=~',    '^A B \S+ \S+$' ],
+    [ "echo A | ./jprefix --suffix --text B --utime",'=~', '^A B \S+ \S+$' ],
 
-    [ "echo B | src/jprefix --quotes",              "eq",    '"B"' ],
-    [ "echo B | src/jprefix --quotes --text A",     "eq",    'A "B"' ],
-    [ "echo B | src/jprefix --quotes --text A --suffix", "eq",    '"B" A' ],
+    [ "echo B | ./jprefix --quotes",              "eq",    '"B"' ],
+    [ "echo B | ./jprefix --quotes --text A",     "eq",    'A "B"' ],
+    [ "echo B | ./jprefix --quotes --text A --suffix", "eq",    '"B" A' ],
 
-    [ "src/jprefix README.md | head -1",            'eq',    '# jprefix' ],
-    [ "src/jprefix --text y README.md | head -1",   'eq',    'y # jprefix' ],
+    [ "./jprefix README.md | head -1",            'eq',    '# jprefix' ],
+    [ "./jprefix --text y README.md | head -1",   'eq',    'y # jprefix' ],
 
-    [ "src/jprefix --nonopt",                       'FAIL', '-'],
+    [ "echo 'a' | ./jprefix -v | tail -1",        '=~',    ' 2 bytes' ],
+
+    [ "./jprefix --nonopt 2> /dev/null",         'FAIL',  '-'],
 );
 
 print "1.." . scalar(@tests) . "\n";    # TAP header
@@ -39,7 +41,7 @@ for my $test (@tests) {
     chomp( my $out = `$cmd` );
     if ($cmp eq "FAIL") {
         $ok = ($? || $!) ? 1 : 0;
-        printf( "%s: test$test_num (exit code expected, got '$?' ($!))\n", $ok ? "PASS" : "FAIL" );
+        printf( "%s: test$test_num (exit code expected, got exit code '$?')\n", $ok ? "PASS" : "FAIL" );
     } else {
         eval "\$ok = \$out $cmp \$expected";
         printf( "%s: test$test_num ('$out' $cmp '$expected')\n", $ok ? "PASS" : "FAIL" );
